656c998a7888cd69f372ba3346d2500a9cc3dc41
Santhosh <santhosh@spectrocloud.com>
2022-10-19 21:16:43 +0530

    containerd proxy env vars set in /etc/default/k3s*
    containerd proxy env vars set in /etc/default/k3s*
    
    service cidr and cluster cidr appended to noProxy

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
-module github.com/c3os-io/c3os/provider-k3s
+module github.com/kairos-io/kairos/provider-k3s
 
 go 1.18
 
 require (
-	github.com/c3os-io/c3os/sdk v0.0.0-20220805230651-57e9b0aebd05
+	github.com/kairos-io/kairos v1.24.3-56.0.20221029012927-185d6a2879c9
 	github.com/mudler/yip v0.0.0-20220725150231-976737b2353c
 	github.com/sirupsen/logrus v1.8.1
 	gopkg.in/yaml.v2 v2.4.0
@@ -11,21 +11,33 @@ require (
 )
 
 require (
+	github.com/atomicgo/cursor v0.0.1 // indirect
+	github.com/avast/retry-go v3.0.0+incompatible // indirect
+	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59 // indirect
 	github.com/chuckpreslar/emission v0.0.0-20170206194824-a7ddd980baf9 // indirect
-	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/denisbrodbeck/machineid v1.0.1 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/gookit/color v1.5.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/itchyny/gojq v0.12.8 // indirect
-	github.com/itchyny/timefmt-go v0.1.3 // indirect
+	github.com/itchyny/gojq v0.12.9 // indirect
+	github.com/itchyny/timefmt-go v0.1.4 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mudler/go-pluggable v0.0.0-20220716112424-189d463e3ff3 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/ginkgo/v2 v2.1.3 // indirect
-	github.com/onsi/gomega v1.19.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pterm/pterm v0.12.41 // indirect
+	github.com/qeesung/image2ascii v1.0.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/twpayne/go-vfs v1.7.2 // indirect
-	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
+	github.com/wayneashleyberry/terminal-dimensions v1.1.0 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467 // indirect
 	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
-replace github.com/c3os-io/c3os v1.22.2 => github.com/c3os-io/c3os v1.24.3-56.0.20220805230651-57e9b0aebd05
--- main.go
+++ main.go
@@ -2,43 +2,57 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
-	"github.com/c3os-io/c3os/sdk/clusterplugin"
+
+	"github.com/kairos-io/kairos/pkg/config"
+	"github.com/kairos-io/kairos/provider-k3s/api"
+	"github.com/kairos-io/kairos/sdk/clusterplugin"
+
 	yip "github.com/mudler/yip/pkg/schema"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
+	"os"
 	"path/filepath"
 	kyaml "sigs.k8s.io/yaml"
+	"strings"
 )
 
 const (
-	configurationPath = "/etc/rancher/k3s/config.d"
+	configurationPath       = "/etc/rancher/k3s/config.d"
+	containerdEnvConfigPath = "/etc/default"
 
 	serverSystemName = "k3s"
 	agentSystemName  = "k3s-agent"
+	K8S_NO_PROXY     = ".svc,.svc.cluster,.svc.cluster.local"
 )
 
-type K3sConfig struct {
-	ClusterInit bool     `yaml:"cluster-init,omitempty"`
-	Token       string   `yaml:"token,omitempty"`
-	Server      string   `yaml:"server,omitempty"`
-	TLSSan      []string `yaml:"tls-san,omitempty"`
-}
+var configScanDir = []string{"/oem", "/usr/local/cloud-config", "/run/initramfs/live"}
 
 func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
-	k3sConfig := K3sConfig{
+	k3sConfig := api.K3sServerConfig{
 		Token: cluster.ClusterToken,
 	}
 
+	var userOptionConfig string
 	switch cluster.Role {
 	case clusterplugin.RoleInit:
 		k3sConfig.ClusterInit = true
 		k3sConfig.TLSSan = []string{cluster.ControlPlaneHost}
+		userOptionConfig = cluster.Options
 	case clusterplugin.RoleControlPlane:
 		k3sConfig.Server = fmt.Sprintf("https://%s:6443", cluster.ControlPlaneHost)
 		k3sConfig.TLSSan = []string{cluster.ControlPlaneHost}
+		userOptionConfig = cluster.Options
 	case clusterplugin.RoleWorker:
 		k3sConfig.Server = fmt.Sprintf("https://%s:6443", cluster.ControlPlaneHost)
+		//Data received from upstream contains config for both control plane and worker. Thus, for worker, config is being filtered
+		//via unmarshal into agent config.
+		var agentCfg api.K3sAgentConfig
+		if err := yaml.Unmarshal([]byte(cluster.Options), &agentCfg); err == nil {
+			out, _ := yaml.Marshal(agentCfg)
+			userOptionConfig = string(out)
+		}
 	}
 
 	systemName := serverSystemName
@@ -51,10 +65,32 @@ func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 		cluster.Options = "{}"
 	}
 
+	_config, _ := config.Scan(config.Directories(configScanDir...))
+
+	if _config != nil {
+		for _, e := range _config.Env {
+			pair := strings.SplitN(e, "=", 2)
+			if len(pair) >= 2 {
+				os.Setenv(pair[0], pair[1])
+			}
+		}
+	}
+
+	_config, _ := config.Scan(config.Directories(configScanDir...))
+
+	if _config != nil {
+		for _, e := range _config.Env {
+			pair := strings.SplitN(e, "=", 2)
+			if len(pair) >= 2 {
+				os.Setenv(pair[0], pair[1])
+			}
+		}
+	}
+
 	var providerConfig bytes.Buffer
 	_ = yaml.NewEncoder(&providerConfig).Encode(&k3sConfig)
 
-	userOptions, _ := kyaml.YAMLToJSON([]byte(cluster.Options))
+	userOptions, _ := kyaml.YAMLToJSON([]byte(userOptionConfig))
 	options, _ := kyaml.YAMLToJSON(providerConfig.Bytes())
 
 	cfg := yip.YipConfig{
@@ -73,6 +109,11 @@ func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 							Path:        filepath.Join(configurationPath, "99_userdata.yaml"),
 							Permissions: 0400,
 							Content:     string(options),
+						},
+						{
+							Path:        filepath.Join(containerdEnvConfigPath, systemName),
+							Permissions: 0400,
+							Content:     proxyEnv(userOptions),
 						},
 					},
 					Commands: []string{
@@ -104,6 +145,56 @@ func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 	}
 
 	return cfg
+}
+
+func proxyEnv(userOptions []byte) string {
+	var proxy []string
+
+	httpProxy := os.Getenv("HTTP_PROXY")
+	httpsProxy := os.Getenv("HTTPS_PROXY")
+	noProxy := getNoProxy(userOptions)
+
+	if len(httpProxy) > 0 {
+		proxy = append(proxy, fmt.Sprintf("HTTP_PROXY=%s", httpProxy))
+		proxy = append(proxy, fmt.Sprintf("CONTAINERD_HTTP_PROXY=%s", httpProxy))
+	}
+
+	if len(httpsProxy) > 0 {
+		proxy = append(proxy, fmt.Sprintf("HTTPS_PROXY=%s", httpsProxy))
+		proxy = append(proxy, fmt.Sprintf("CONTAINERD_HTTPS_PROXY=%s", httpsProxy))
+	}
+
+	if len(noProxy) > 0 {
+		proxy = append(proxy, fmt.Sprintf("NO_PROXY=%s", noProxy))
+		proxy = append(proxy, fmt.Sprintf("CONTAINERD_NO_PROXY=%s", noProxy))
+	}
+
+	return strings.Join(proxy, "\n")
+}
+
+func getNoProxy(userOptions []byte) string {
+
+	noProxy := os.Getenv("NO_PROXY")
+
+	var data map[string]interface{}
+	err := json.Unmarshal(userOptions, &data)
+	if err != nil {
+		fmt.Println("error while unmarshalling user options", err)
+	}
+
+	if data != nil {
+		cluster_cidr := data["cluster-cidr"].(string)
+		service_cidr := data["service-cidr"].(string)
+
+		if len(cluster_cidr) > 0 {
+			noProxy = noProxy + "," + cluster_cidr
+		}
+		if len(service_cidr) > 0 {
+			noProxy = noProxy + "," + service_cidr
+		}
+	}
+	noProxy = noProxy + "," + K8S_NO_PROXY
+	return noProxy
 }
 
 func main() {
