517f1bab82d0eb7c6d2d1604fb3eaaf2572b091b
Tyler Gillson <tyler.gillson@gmail.com>
2023-10-06 11:21:53 -0600

    create ssh_keys table; fake marmot follower init

--- scripts/prepare_sqlite.sh
+++ scripts/prepare_sqlite.sh
@@ -6,24 +6,33 @@ db_dir="/var/lib/rancher/k3s/server/db"
 
 mkdir -p $db_dir
 
-# SQL commands to create the 'kine' table
+# SQL commands to create the 'kine' and 'ssh_keys' tables
 sql_commands="
 CREATE TABLE IF NOT EXISTS kine (
-				id INTEGER PRIMARY KEY AUTOINCREMENT,
-				name INTEGER,
-				created INTEGER,
-				deleted INTEGER,
-				create_revision INTEGER,
-				prev_revision INTEGER,
-				lease INTEGER,
-				value BLOB,
-				old_value BLOB
-			);
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name INTEGER,
+	created INTEGER,
+	deleted INTEGER,
+	create_revision INTEGER,
+	prev_revision INTEGER,
+	lease INTEGER,
+	value BLOB,
+	old_value BLOB
+);
 CREATE INDEX IF NOT EXISTS kine_name_index ON kine (name);
 CREATE INDEX IF NOT EXISTS kine_name_id_index ON kine (name,id);
 CREATE INDEX IF NOT EXISTS kine_id_deleted_index ON kine (id,deleted);
 CREATE INDEX IF NOT EXISTS kine_prev_revision_index ON kine (prev_revision);
 CREATE UNIQUE INDEX IF NOT EXISTS kine_name_prev_revision_uindex ON kine (name, prev_revision);
+
+CREATE TABLE IF NOT EXISTS ssh_keys (
+	id INTEGER NOT NULL PRIMARY KEY,
+	host TEXT NOT NULL,
+	public_key TEXT NOT NULL
+);
+CREATE INDEX IF NOT EXISTS ssh_keys_host_index ON ssh_keys (host);
+CREATE INDEX IF NOT EXISTS ssh_keys_host_id_index ON ssh_keys (host,id);
+
 PRAGMA wal_checkpoint(TRUNCATE);
 "
 
@@ -31,4 +40,4 @@ PRAGMA wal_checkpoint(TRUNCATE);
 echo "$sql_commands" | sqlite3 "$db_dir/state.db"
 
 # Print a success message
-echo "Table 'kine' created successfully"
+echo "sqlite database initialized successfully"
