41f2ce10e121b31e5fdcc0431fc16a142e63889b
Tyler Gillson <tyler.gillson@gmail.com>
2024-01-19 10:59:07 -0500

    refactor: make provider options strings
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -7,27 +7,25 @@ const (
 	ImportK3sImages       = "Import K3s Images"
 )
 
-// ProviderOptions are keys provider-k3s supports if present in Cluster.ProviderOptions from the Kairos SDK.
-type ProviderOption string
-
+// The following are keys provider-k3s supports if present in Cluster.ProviderOptions from the Kairos SDK.
 const (
 	// K3s bind address
-	BindAddress ProviderOption = "bind-address"
+	BindAddress string = "bind-address"
 
 	// If value == 'yes', provider-k3s will use etcd for its datastore.
 	// If value == 'no' and DatastoreEndpoint is not defined, the sqlite datastore will be used.
 	// If value == 'no' and DatastoreEndpoint is defined, a custom datastore will be used.
-	ClusterInit ProviderOption = "cluster-init"
+	ClusterInit string = "cluster-init"
 
 	// A PostgreSQL, MySQL, NATS, or etcd connection string. Used to describe the connection to the datastore.
-	DatastoreEndpoint ProviderOption = "datastore-endpoint"
+	DatastoreEndpoint string = "datastore-endpoint"
 
 	// TLS Certificate Authority (CA) file used to help secure communication with the datastore.
-	DatastoreCaFile ProviderOption = "datastore-cafile"
+	DatastoreCaFile string = "datastore-cafile"
 
 	// TLS certificate file used for client certificate based authentication to your datastore.
-	DatastoreCertFile ProviderOption = "datastore-certfile"
+	DatastoreCertFile string = "datastore-certfile"
 
 	// TLS key file used for client certificate based authentication to your datastore.
-	DatastoreKeyFile ProviderOption = "datastore-keyfile"
+	DatastoreKeyFile string = "datastore-keyfile"
 )
