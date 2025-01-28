153d95162a07d5dd7b53c51e0294eab750ab85a4
Nianyu Shen <nianyu@spectrocloud.com>
2025-01-27 21:43:30 -0800

    fix: add log rotation and version in logging (#100)
    * fix: add log rotation and version in logging
    
    Signed-off-by: Nianyu Shen <xiaoyu9964@gmail.com>
    
    * ci: use 22.04 ubuntu runner
    
    Signed-off-by: Nianyu Shen <xiaoyu9964@gmail.com>
    
    ---------
    
    Signed-off-by: Nianyu Shen <xiaoyu9964@gmail.com>

--- main.go
+++ main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"os"
-
 	"github.com/kairos-io/kairos-sdk/clusterplugin"
 	"github.com/mudler/go-pluggable"
 	"github.com/sirupsen/logrus"
 
+	"github.com/kairos-io/provider-k3s/pkg/log"
 	"github.com/kairos-io/provider-k3s/pkg/provider"
 )
 
 func main() {
-	f, err := os.OpenFile("/var/log/provider-k3s.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
-	}
-	logrus.SetOutput(f)
+	log.InitLogger("/var/log/provider-k3s.log")
 
 	plugin := clusterplugin.ClusterPlugin{
 		Provider: provider.ClusterProvider,
