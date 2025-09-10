b245d61850314a24ef3cd9985543e1db69d8bdc6
Nianyu Shen <xiaoyu9964@gmail.com>
2025-09-10 15:34:50 -0700

    fix: make import.sh POSIX compatiable

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 set -x
 CONTENT_PATH=$1
 mkdir -p /var/lib/rancher/k3s/agent/images
-for tarfile in $(find $CONTENT_PATH -name "*.tar" -type f)
-do
+
+find -L "$CONTENT_PATH" -name "*.tar" -type f | while read -r tarfile; do
   cp $tarfile /var/lib/rancher/k3s/agent/images
 done
